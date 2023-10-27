a3034886dff06402b8f6599ed7eb6bd92023ab28
Omar Karam <krm3762a@ensfsi.onmicrosoft.com>
2023-10-27 10:49:22 +0200

    tp test

--- test/controleur/ControlEmmenagerTest.java
+++ test/controleur/ControlEmmenagerTest.java
@@ -7,7 +7,22 @@ import org.junit.jupiter.api.Test;
 class ControlEmmenagerTest {
 
 	@Test
-	void test() {
+	void testControlEmmenager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsHabitant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAjouterDruide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAjouterGaulois() {
 		fail("Not yet implemented");
 	}
 
